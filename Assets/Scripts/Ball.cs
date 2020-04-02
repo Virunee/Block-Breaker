17d7550e80f6fda38cd2b7686d59c6b089f2c003
Virunee <viravongv@gmail.com>
2020-04-02 23:08:05 +0100

    Initial commit
    I forgot to commit whilst I was working on it

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //Configuration parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xLaunch = 2f;
+    [SerializeField] float yLaunch = 15f;
+
+    //State
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Calculate the initial distance between the paddle and the ball
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle(hasStarted);
+            LaunchOnClick();
+        }
+        
+    }
+
+    private void LockBallToPaddle(bool startState)
+    {
+            Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+            transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xLaunch, yLaunch);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 2f;
+    [SerializeField] float maxX = 14.35f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosXInUnits = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+
+        //compact way of storing X and Y co-ordinates
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePosXInUnits, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
